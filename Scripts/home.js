1caa7ae8e7039541b5b17f1828c7b1bc708ddc1b
Karandeep Singh Grewal <kgrewal2@asu.edu>
2020-03-22 14:30:03 -0700

    Added: 3 Column Layout

--- Scripts/home.js
+++ Scripts/home.js
@@ -2,9 +2,9 @@ var interval;
 var seconds = 0;
 
 
-function startTimer(time) {
+function startTimer() {
     clearInterval(interval);
-    seconds = time * 60 - 1;
+    seconds = 15;
     interval = setInterval(changeTimeLabel, 1000);
 }
 
@@ -19,4 +19,56 @@ function changeTimeLabel() {
         clearInterval(interval);
     if (seconds < 10)
         timeLabel.style.color = "red";
+}
+
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    myNodelist[i].appendChild(span);
+}
+
+
+var close = document.getElementsByClassName("close");
+for (i = 0; i < close.length; i++) {
+    close[i].onclick = function () {
+        var div = this.parentElement;
+        div.style.display = "none";
+    }
+}
+
+
+var list = document.querySelector('ul');
+list.addEventListener('click', function (ev) {
+    if (ev.target.tagName === 'LI') {
+        ev.target.classList.toggle('checked');
+    }
+}, false);
+
+
+function newElement() {
+    var li = document.createElement("li");
+    var inputValue = document.getElementById("taskTitle").value;
+    var t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    if (!(inputValue !== '')) {
+        document.getElementById("tasksList").appendChild(li);
+    }
+    document.getElementById("taskTitle").value = "";
+
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
+
+    for (i = 0; i < close.length; i++) {
+        close[i].onclick = function () {
+            var div = this.parentElement;
+            div.style.display = "none";
+        }
+    }
 }
