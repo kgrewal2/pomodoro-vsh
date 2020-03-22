368972fdef7a346d0350dc5d39f459381a7c700f
Karandeep Singh Grewal <kgrewal2@asu.edu>
2020-03-22 02:22:11 -0700

    Added: Pomodoro, Short Break and Long Break buttons

--- Scripts/home.js
+++ Scripts/home.js
@@ -2,24 +2,21 @@ var interval;
 var seconds = 0;
 
 
-function startPomodoro() {
-    seconds = 25*60-1;
+function startTimer(time) {
+    clearInterval(interval);
+    seconds = time * 60 - 1;
     interval = setInterval(changeTimeLabel, 1000);
 }
 
 function changeTimeLabel() {
     let timeLabel = document.getElementById("time-label");
-    let min = Math.floor(seconds/60);
-    let sec = ("0"+seconds%60).slice(-2);
-        seconds--;
-        if (!seconds) {
-            clearInterval(interval);
-            timeLabel.innerHTML = "Done"
-        }
-        if(seconds<10){
-            timeLabel.style.color = "red";
-            timeLabel.innerHTML = seconds;}
-        else{
-            timeLabel.innerHTML = min+":"+sec;
-        }
+
+    let min = ("0" + Math.floor(seconds / 60)).slice(-2);
+    let sec = ("0" + seconds % 60).slice(-2);
+    seconds--;
+    timeLabel.innerHTML = min + ":" + sec;
+    if (seconds === -1)
+        clearInterval(interval);
+    if (seconds < 10)
+        timeLabel.style.color = "red";
 }
