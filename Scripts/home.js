110f15dd946967ef8b990827565fec9b85d226e3
Stephanie Miranda <smirand6@asu.edu>
2020-03-23 12:15:20 -0600

    Updated the layout of the app...
    Added sound, but it's not yet working, keeps throwing errors.

--- Scripts/home.js
+++ Scripts/home.js
@@ -1,25 +1,51 @@
 var interval;
 var seconds = 0;
+let tickingSoundMP3 = new sound("./pomodoro-vsh/resources/Ticking_Clock-KevanGC-1934595011.mp3");
+let tickingSoundWAV = new sound("./pomodoro-vsh/resources/Ticking_Clock-KevanGC-1934595011.wav");
 
 /*Start Timer*/
 function startTimer(time) {
     clearInterval(interval);
-    seconds = time * 60;
+    seconds = 15;//time * 60;
     interval = setInterval(changeTimeLabel, 1000);
+}
+
+
+/*Play ticking sound.*/
+function playsound(snd){
+    this.sound = document.createElement("audio");
+    this.sound.src = snd;
+    this.sound.setAttribute("preload", "auto");
+    this.sound.setAttribute("controls", "none");
+    this.sound.style.display = "none";
+    document.body.appendChild(this.sound);
+    this.play = function(){
+        this.sound.play();
+    }
+    this.stop = function(){
+        this.sound.pause();
+    }
 }
 
 /*Change time label*/
 function changeTimeLabel() {
     let timeLabel = document.getElementById("time-label");
+    var selectedSession = 4;
 
     let min = ("0" + Math.floor(seconds / 60)).slice(-2);
     let sec = ("0" + seconds % 60).slice(-2);
+    let hr = (min * 60)/selectedSession;
     seconds--;
-    timeLabel.innerHTML = min + ":" + sec;
+    timeLabel.innerHTML = hr + ":" + min + ":" + sec;
     if (seconds === -1)
         clearInterval(interval);
     if (seconds < 10)
         timeLabel.style.color = "red";
+        /*Start flashing and make noise here @ ~ 10 seconds left.*/
+        //tickingSoundMP3.play();
+        //tickingSoundWAV.play();
+        playsound(tickingSoundMP3);
+        playsound(tickingSoundWAV);
 }
 
 
